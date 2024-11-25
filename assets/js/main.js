56b3dd9774e81bc8e687a9c927b8b9c99c2d206e
abdfahmawe <fahmawe09@gmail.com>
2024-11-26 00:42:04 +0200

    make the first section

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,15 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+// var swiper = new Swiper(".mySwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
